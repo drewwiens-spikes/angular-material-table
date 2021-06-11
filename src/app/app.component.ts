572416b4b83f43f58bb025372132753f71025fbf
Drew Wiens <adwiens@users.noreply.github.com>
2021-06-11 14:27:40 -0500

    create basic mat-table w/ random word in each cell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import * as casual from 'casual-browserify';
+import { range, chunk, mapKeys } from 'lodash';
+
+type Row = Record<string, string>;
+
+const NUM_ROWS = 1 * 1000;
+const NUM_COLS = 22;
+const WORDS = casual.array_of_words(NUM_ROWS * NUM_COLS);
+const ROWS = chunk(WORDS, NUM_COLS);
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-material-table';
+  // Convert number keys to string keys:
+  displayedColumns = range(NUM_COLS).map(String);
+  rows: Row[] = ROWS.map(r => mapKeys(r, (_v, k) => String(k)));
 }
